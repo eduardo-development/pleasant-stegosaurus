527b62bc9ff0f81afc93c80d583cf9b4adf3b21a
stackbit-projects <projects@stackbit.com>
2020-06-19 09:51:55 +0100

    Update configuration

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,8 +4,8 @@ set -e
 set -o pipefail
 set -v
 
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/pull > /dev/null
-npx @stackbit/stackbit-pull --stackbit-pull-api-url=__STACKBIT_PULL_API_URL__
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/ssgbuild > /dev/null
+curl -s -X POST https://249701722381.ngrok.io/project/5eec7c94b68f3c811c34a5ad/webhook/build/pull > /dev/null
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://249701722381.ngrok.io/pull/5eec7c94b68f3c811c34a5ad
+curl -s -X POST https://249701722381.ngrok.io/project/5eec7c94b68f3c811c34a5ad/webhook/build/ssgbuild > /dev/null
 gatsby build
-curl -s -X POST __STACKBIT_WEBHOOK_URL__/publish > /dev/null
+curl -s -X POST https://249701722381.ngrok.io/project/5eec7c94b68f3c811c34a5ad/webhook/build/publish > /dev/null
